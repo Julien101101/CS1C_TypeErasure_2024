0f9aa99a3bd503965e5318e6877c2d31a707bbb8
Julien Savary <juliensavary2001@gmail.com>
2024-07-31 17:17:42 -0700

    final changes to fix client code pt2

--- account_impl.h
+++ account_impl.h
@@ -3,21 +3,42 @@
 
 #include "accounts.h"
 
-template <typename T>
-class AccountImpl : public Account {
-public:
-    AccountImpl(T account) : account_(std::move(account)) {}
-    
-    void getAccountInfo() const override {
-        account_.getAccountInfo();
-    }
-    
-    void performOperations() override {
-        account_.performOperations();
+struct TypeErased
+{
+    template<typename T>
+    TypeErased(T&& value) { *this = value; }
+
+    template<typename T>
+    TypeErased& operator=(T&& value)
+    { 
+        m_value.reset(new Model<T>(value));
+        return *this;
     }
 
+    void getAccountInfo() const
+    { m_value->getAccountInfo();}
+    
+    void performOperations()
+    { m_value->performOperations(); }
 private:
-    T account_;
+    struct Concept {
+        virtual ~Concept() {}
+        virtual void getAccountInfo() const = 0;
+        virtual void performOperations() = 0;
+};
+    template<typename T>
+    struct Model : Concept {
+        Model(T const & value) : m_val(value) {}
+
+        void getAccountInfo() const override
+        { m_val.getAccountInfo(); }
+        void performOperations() override
+        { m_val.performOperations(); }
+
+        T m_val;
+    };
+private:
+    std::unique_ptr<Concept> m_value;
 };
 
 #endif // ACCOUNT_IMPL_H
